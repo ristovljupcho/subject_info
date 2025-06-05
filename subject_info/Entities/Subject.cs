ec54bc138a8e7bc7561a01b9163385ed4d6f5693
ristovljupcho <ristov.ljupcho@gmail.com>
2025-06-05 23:21:02 +0200

    Removed method explanation.

--- subject_info/Entities/Subject.cs
+++ subject_info/Entities/Subject.cs
@@ -25,26 +25,11 @@ namespace subject_info.Entities
             Literature = new List<Literature>();
         }
 
-        /// <summary>
-        /// Helper method to get literature details as a formatted string.
-        /// </summary>
-        /// <returns>
-        /// string - containing details of all literature associated with the subject,
-        /// or a default message if none exist.
-        /// </returns>
         private string GetLiteratureDetails()
         {
             return Literature.Count > 0 ? string.Join("\n", Literature.Select(l => l.GetDetails())) : "No literature available";
         }
 
-        /// <summary>
-        /// Gets a formatted string containing all details of the subject, 
-        /// including name, description, weekly classes, additional information,
-        /// and a list of associated literature.
-        /// </summary>
-        /// <returns>
-        /// A string with all subject details formatted for display.
-        /// </returns>
         public virtual string GetDetails()
         {
             var stringBuilder = new StringBuilder();
